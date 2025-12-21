dd314161932e4286354eef006fd1dfb88d8f5a5c
narmuni123 <narmuni123@gmail.com>
2025-12-22 04:49:58 +0530

    Added category layer - added get all category

---
+++ src/main/java/com/muni/muni_ecom/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package com.muni.muni_ecom.controller;
+
+import com.muni.muni_ecom.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("api/public/categories")
+    public List<Category> getAllCategories() {
+        return  categories;
+    }
+}
