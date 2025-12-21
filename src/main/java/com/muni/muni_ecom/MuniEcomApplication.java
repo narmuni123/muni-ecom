c06c46afc5ea7a9f17a8a6f8da402f50c9f2a1cd
narmuni123 <narmuni123@gmail.com>
2025-12-21 22:25:24 +0530

    Initial commit

---
+++ src/main/java/com/muni/muni_ecom/MuniEcomApplication.java
@@ -0,0 +1,13 @@
+package com.muni.muni_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuniEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuniEcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muni/muni_ecom/MuniEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muni.muni_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuniEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
